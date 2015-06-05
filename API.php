b81ceea4c6d99eeb4bfb3eb391f14c0d4aca8506
Alex Markessinis <markea@almyz125.com>
2015-06-05 13:49:22 -0400

    sending 404 when headers are not corect

--- API.php
+++ API.php
@@ -8,10 +8,10 @@
  */
 
 if ($data_sent = ($_GET['update'] | $_POST['update'])) {
-
     echo json_encode($data_sent);
-
 } else {
-    echo 'Hello';
+    header('HTTP/1.0 404 Not Found');
+    echo "<h1>404 Not Found</h1>";
+    echo "The page that you have requested could not be found.";
+    exit();
 }
-
