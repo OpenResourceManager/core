96a0806d948603d843f4f2edfb4dfe6e6c7152fb
Alex Markessinis <markea@almyz125.com>
2015-06-05 22:43:17 -0400

    api hhandles write access

--- api.php
+++ api.php
@@ -25,17 +25,23 @@ if (isset($_POST['X-Authorization']) && $apiKey = $api->checkAPIKey($_POST['X-Au
     // Unset the api key variable
     unset($apiKey);
     // Define API action sets
-    if ($_POST['action'] === 'write' && $canWrite) { // If we want to write and can write
+    if (isset($_POST['action'])) { // If we have the action key in our post array
+        if ($_POST['action'] === 'write' && $canWrite) { // If we want to write and can write
 
-    } elseif ($_POST['action'] === 'read') { // If we want to read
+        } elseif ($_POST['action'] === 'read') { // If we want to read
 
-    } elseif ($_POST['action'] === 'write' && !$canWrite) { // If we want to write and can NOT write
-        header('HTTP/1.1 401 Forbidden');
-        echo json_encode(array('success' => false, 'message' => 'Insufficient privileges to write.'));
-    } elseif (!isset($_POST['action']) || $_POST['action'] !== ('read' || 'write')) { // Either action was not supplied or an invalid one was
+        } elseif ($_POST['action'] === 'write' && !$canWrite) { // If we want to write and can NOT write
+            header('HTTP/1.1 401 Forbidden');
+            echo json_encode(array('success' => false, 'message' => 'Insufficient privileges to write.'));
+        } elseif ($_POST['action'] !== ('read' || 'write')) { // Invalid action was not supplied
+            header('HTTP/1.1 404 Not Found');
+            echo json_encode(array('success' => false, 'message' => 'Action not defined.'));
+        }
+    } else { // There is no action key value pair
         header('HTTP/1.1 404 Not Found');
-        echo json_encode(array('success' => false, 'message' => 'Action not defined.'));
+        echo json_encode(array('success' => false, 'message' => 'Action not supplied.'));
     }
+
     exit();
 } else {
     // Throw a 401 unauthorized, since the app is not authorized
