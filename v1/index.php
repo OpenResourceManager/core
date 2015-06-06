795f6743fb9ce9542bf4191166e32256e289b67b
Alex Markessinis <markea@almyz125.com>
2015-06-06 12:53:36 -0400

    root of each group should show methods

--- v1/index.php
+++ v1/index.php
@@ -21,52 +21,43 @@ $api = new API();
 // Check the API authorization
 if ($slim->request->headers->get('X-Authorization') && $apiKey = $api->checkAPIKey($slim->request->headers->get('X-Authorization'))) {
 
-    $slim->group('/user', function () use ($slim, $api) {
+    $slim->group('/user', function () use ($slim, $api, $apiKey) {
 
-        $slim->get('/id/:id', function ($id) use ($api) {
-
-        });
-
-        $slim->get('/id_num/:id_num', function ($id_num) use ($api) {
+        $slim->get('/id/:id', function ($id) use ($api, $apiKey) {
 
         });
 
-        $slim->get('/username/:username', function ($username) use ($api) {
+        $slim->get('/id_num/:id_num', function ($id_num) use ($api, $apiKey) {
 
         });
 
-        $slim->get('/:limit', function ($limit) use ($api) {
-
-        });
-    });
-
-    $slim->group(('/role'), function () use ($slim, $api) {
-
-        $slim->get('/id/:id', function ($id) use ($api) {
+        $slim->get('/username/:username', function ($username) use ($api, $apiKey) {
 
         });
 
-        $slim->get('/:limit', function ($limit) use ($api) {
-
-        });
-    });
-
-    $slim->group(('/building'), function () use ($slim, $api) {
-
-        $slim->get('/id/:id', function ($id) use ($api) {
+        $slim->get('/:limit', function ($limit) use ($api, $apiKey) {
 
         });
 
-        $slim->get('/datatel_code/:datatel_code', function ($datatel_code) use ($api) {
-
-        });
-
-        $slim->get('/:limit', function ($limit) use ($api) {
-
+        $slim->get('/', function () use ($api, $apiKey) {
+            return json_encode(array(
+                'application' => $apiKey['app'],
+                'success' => true,
+                'result' => array(),
+                'methods' => array(
+                    'get' => array(
+                        '/id/:id',
+                        '/id_num/:id_num',
+                        '/username/:username',
+                        '/:limit'
+                    ),
+                    'post' => array()
+                )
+            ));
         });
     });
 
-    $slim->group(('/campus'), function () use ($slim, $api) {
+    $slim->group(('/role'), function () use ($slim, $api, $apiKey) {
 
         $slim->get('/id/:id', function ($id) use ($api) {
 
@@ -79,9 +70,82 @@ if ($slim->request->headers->get('X-Authorization') && $apiKey = $api->checkAPIK
         $slim->get('/:limit', function ($limit) use ($api) {
 
         });
+        $slim->get('/', function () use ($api, $apiKey) {
+            return json_encode(array(
+                'application' => $apiKey['app'],
+                'success' => true,
+                'result' => array(),
+                'methods' => array(
+                    'get' => array(
+                        '/id/:id',
+                        '/datatel_code/:datatel_code',
+                        '/:limit'
+                    ),
+                    'post' => array()
+                )
+            ));
+        });
     });
 
+    $slim->group(('/building'), function () use ($slim, $api, $apiKey) {
 
+        $slim->get('/id/:id', function ($id) use ($api) {
+
+        });
+
+        $slim->get('/datatel_code/:datatel_code', function ($datatel_code) use ($api) {
+
+        });
+
+        $slim->get('/:limit', function ($limit) use ($api) {
+
+        });
+        $slim->get('/', function () use ($api, $apiKey) {
+            return json_encode(array(
+                'application' => $apiKey['app'],
+                'success' => true,
+                'result' => array(),
+                'methods' => array(
+                    'get' => array(
+                        '/id/:id',
+                        '/datatel_code/:datatel_code',
+                        '/:limit'
+                    ),
+                    'post' => array()
+                )
+            ));
+        });
+    });
+
+    $slim->group(('/campus'), function () use ($slim, $api, $apiKey) {
+
+        $slim->get('/id/:id', function ($id) use ($api) {
+
+        });
+
+        $slim->get('/datatel_code/:datatel_code', function ($datatel_code) use ($api) {
+
+        });
+
+        $slim->get('/:limit', function ($limit) use ($api) {
+
+        });
+        $slim->get('/', function () use ($api, $apiKey) {
+            return json_encode(array(
+                'application' => $apiKey['app'],
+                'success' => true,
+                'result' => array(),
+                'methods' => array(
+                    'get' => array(
+                        '/id/:id',
+                        '/datatel_code/:datatel_code',
+                        '/:limit'
+                    ),
+                    'post' => array()
+                )
+            ));
+        });
+    });
     $slim->run();
 } else {
     // Throw a 401 unauthorized, since the app is not authorized
