0da3aea6908d5094c2e3b20fb7a9a13d35edfa83
Alex Markessinis <markea@almyz125.com>
2015-06-06 10:50:25 -0400

    testing slim

--- v1/api.php
+++ v1/api.php
@@ -5,3 +5,10 @@
  * Date: 6/6/15
  * Time: 10:40 AM
  */
+require dirname(dirname(__FILE__)) . 'vendor/autoload.php';
+
+$app = new \Slim\Slim();
+$app->get('/hello/:name', function ($name) {
+    echo "Hello, " . $name;
+});
+$app->run();
