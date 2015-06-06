02fd1a95701df54c1ab2b889c2dd029b750ae834
Alex Markessinis <markea@almyz125.com>
2015-06-06 17:22:38 -0400

    added ability to post to user portion of the api

--- v1/index.php
+++ v1/index.php
@@ -53,7 +53,7 @@ if ($slim->request->headers->get('X-Authorization') && $apiKey = $api->checkAPIK
          *      }
          */
         $slim->post('/idnum/:idnum', function ($idnum) use ($api, $apiKey, $mysqli, $MySQLiHelper, $slim) {
-            if ($apiKey['write'] === 1) {
+            if ($apiKey['write'] == 1) {
                 $output = $api->updateRecordSageID($idnum, $slim->request->post(), $mysqli, $MySQLiHelper);
                 $output['application'] = $apiKey['app'];
                 echo json_encode($output);
