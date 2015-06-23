cf26de86dca4c75feb58d54112d42e0e6fd81009
Alex Markessinis <markea@almyz125.com>
2015-06-23 15:45:07 -0400

    added users table migration

---
+++ lumen/database/migrations/2015_06_23_193628_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('sageid')->unique();
+            $table->boolean('active');
+            $table->string('name_first');
+            $table->string('name_middle')->nullable();
+            $table->string('name_last');
+            $table->string('username')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
