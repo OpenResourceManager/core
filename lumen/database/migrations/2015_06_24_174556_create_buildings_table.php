db1b85554d73d49922295007139353ba7facfc04
Alex Markessinis <markea@almyz125.com>
2015-06-24 13:55:53 -0400

    added migrations for inital tables

---
+++ lumen/database/migrations/2015_06_24_174556_create_buildings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBuildingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('buildings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('campus');
+            $table->string('code');
+            $table->string('name');
+            $table->timestamps();
+
+            $table->foreign('campus')->references('id')->on('campuses');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('buildings');
+    }
+}
---
+++ lumen/database/migrations/2015_06_24_174652_create_programs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProgramsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('programs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('department');
+            $table->string('code');
+            $table->string('name');
+            $table->timestamps();
+            $table->foreign('department')->references('id')->on('departments');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('programs');
+    }
+}
