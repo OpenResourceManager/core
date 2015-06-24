6fe6127fbd5690988be4df2a75078d4f7faf415b
Alex Markessinis <markea@almyz125.com>
2015-06-24 16:03:01 -0400

    fixing migrations

---
+++ lumen/database/migrations/2015_06_23_193628_create_tables.php
@@ -0,0 +1,87 @@
+<?php use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('sageid')->unique();
+            $table->boolean('active');
+            $table->string('name_first');
+            $table->string('name_middle')->nullable();
+            $table->string('name_last');
+            $table->string('username')->unique();
+            $table->timestamps();
+        });
+
+        Schema::create('roles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('code');
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('campuses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('code');
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('buildings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('campus');
+            $table->string('code');
+            $table->string('name');
+            $table->timestamps();
+            $table->foreign('campus')->references('id')->on('campuses');
+        });
+
+        Schema::create('departments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->boolean('academic');
+            $table->string('code');
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('programs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('department');
+            $table->string('code');
+            $table->string('name');
+            $table->timestamps();
+            $table->foreign('department')->references('id')->on('departments');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('buildings', function (Blueprint $table) {
+            $table->dropForeign('buildings_department_id_foreign');
+        });
+
+        Schema::table('programs', function (Blueprint $table) {
+            $table->dropForeign('programs_campus_id_foreign');
+        });
+
+        Schema::drop('users');
+        Schema::drop('roles');
+        Schema::drop('campuses');
+        Schema::drop('buildings');
+        Schema::drop('departments');
+        Schema::drop('programs');
+    }
+}
