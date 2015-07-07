f62a2e56b783dc28c9d762c51c556cb2f8d73ada
Alex Markessinis <markea@almyz125.com>
2015-07-07 10:41:53 -0400

    created campus seeder

---
+++ lumen/database/seeds/CampusTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use App\Campus;
+
+/**
+ * Created by PhpStorm.
+ * User: melon
+ * Date: 7/7/15
+ * Time: 10:36 AM
+ */
+class CampusTableSeeder extends Seeder
+{
+
+    public function run()
+    {
+        Model::unguard();
+
+        // Create an array of campuses
+        $campuses = array(
+            'TRY' => 'Russell Sage College',
+            'ALB' => 'Sage College of Albany'
+        );
+
+        // Loop through the array and save the campuses
+        foreach ($campuses as $code => $name) {
+            $campus = new Campus();
+            $campus->code = $code;
+            $campus->name = $name;
+            $campus->save();
+        }
+
+    }
+
+}
