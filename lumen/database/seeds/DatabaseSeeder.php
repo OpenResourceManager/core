7ace37ade2745c07c65f03a31491f90d9ac9955f
Alex Markessinis <markea@almyz125.com>
2015-07-07 11:05:41 -0400

    created buildings table seeder

--- lumen/database/seeds/DatabaseSeeder.php
+++ lumen/database/seeds/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call('UserTableSeeder');
         $this->call('RoleTableSeeder');
         $this->call('CampusTableSeeder');
+        $this->call('BuildingTableSeeder');
     }
 
 }
