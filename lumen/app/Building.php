3971ef5646e1804b19ba2e3c0d7d5b5548def15b
Alex Markessinis <markea@almyz125.com>
2015-07-07 10:32:20 -0400

    created building model

---
+++ lumen/app/Building.php
@@ -0,0 +1,15 @@
+<?php namespace App;
+
+/**
+ * Created by PhpStorm.
+ * User: melon
+ * Date: 7/7/15
+ * Time: 10:30 AM
+ */
+
+use Illuminate\Database\Eloquent\Model;
+
+class Building extends Model
+{
+    protected $table = 'buildings';
+}
