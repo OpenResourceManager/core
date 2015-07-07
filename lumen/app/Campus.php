e88159a652b0021682826c399586b0c8c1e39c30
Alex Markessinis <markea@almyz125.com>
2015-07-07 10:29:16 -0400

    created campus model

---
+++ lumen/app/Campus.php
@@ -0,0 +1,15 @@
+<?php namespace App;
+
+/**
+ * Created by PhpStorm.
+ * User: melon
+ * Date: 7/7/15
+ * Time: 10:19 AM
+ */
+
+use Illuminate\Database\Eloquent\Model;
+
+class Campus extends Model
+{
+    protected $table = 'campuses';
+}
