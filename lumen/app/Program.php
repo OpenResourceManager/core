89289a0a445a98ff9ae2cbfcbc9cccfc46dfee43
Alex Markessinis <markea@almyz125.com>
2015-07-07 10:34:38 -0400

    created programs model

---
+++ lumen/app/Program.php
@@ -0,0 +1,15 @@
+<?php namespace App;
+
+/**
+ * Created by PhpStorm.
+ * User: melon
+ * Date: 7/7/15
+ * Time: 10:34 AM
+ */
+
+use Illuminate\Database\Eloquent\Model;
+
+class Program extends Model
+{
+    protected $table = 'programs';
+}
