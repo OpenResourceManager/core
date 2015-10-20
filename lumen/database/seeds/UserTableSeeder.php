81fe898c9220d948118e4a472e6b29c15aeb37af
Alex Markessinis <markea@almyz125.com>
2015-10-19 23:24:16 -0400

    updated user table seeder with new fields

--- lumen/database/seeds/UserTableSeeder.php
+++ lumen/database/seeds/UserTableSeeder.php
@@ -18,9 +18,9 @@ class UserTableSeeder extends Seeder
         Model::unguard();
 
         $users = array(
-            array(970620, true, 'Alex', 'L', 'Markessinis', 'markea'),
-            array(970621, true, 'Adam', null, 'Starnes', 'starna'),
-            array(970623, true, 'John', null, 'Harris', 'harrij8')
+            array(970620, true, 'Mr.', 'Alex', 'L', 'Markessinis', null, 'markea'),
+            array(970621, true, 'Mr.', 'Adam', null, 'Starnes', null, 'starna'),
+            array(970623, true, null, 'John', null, 'Harris', 'Jon', 'harrij8')
         );
 
 
@@ -28,10 +28,12 @@ class UserTableSeeder extends Seeder
             $user = new User();
             $user->sageid = $userArr[0];
             $user->active = $userArr[1];
-            $user->name_first = $userArr[2];
-            $user->name_middle = $userArr[3];
-            $user->name_last = $userArr[4];
-            $user->username = $userArr[5];
+            $user->name_prefix = $userArr[2];
+            $user->name_first = $userArr[3];
+            $user->name_middle = $userArr[4];
+            $user->name_last = $userArr[5];
+            $user->name_phonetic = $userArr[6];
+            $user->username = $userArr[7];
             $user->save();
         }
 
