3f4ba1534ef0d67377953b746eca065a5944d8cf
Alex Markessinis <markea@almyz125.com>
2015-10-19 22:42:37 -0400

    upped max length for department code

--- lumen/app/Http/Controllers/DepartmentController.php
+++ lumen/app/Http/Controllers/DepartmentController.php
@@ -74,7 +74,7 @@ class DepartmentController extends BaseController
 
         $validator = Validator::make($request->all(), [
             'academic' => 'boolean|required|max:5|min:1',
-            'code' => 'string|required|max:10|min:3|unique:departments',
+            'code' => 'string|required|max:50|min:3|unique:departments',
             'name' => 'string|required|max:30|min:3'
         ]);
 
