675a2388250d14a59335948cece31c8472ee252e
Alex Markessinis <markea@almyz125.com>
2015-10-19 23:04:01 -0400

    rooms can take a post request now

--- lumen/app/Http/Controllers/RoomController.php
+++ lumen/app/Http/Controllers/RoomController.php
@@ -44,4 +44,61 @@ class RoomController extends BaseController
         }
     }
 
+    /**
+     * @param Request $request
+     * @return string
+     */
+    public function post(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'user' => 'integer|required|max:11|min:1',
+            'building' => 'integer|required|max:11|min:1',
+            'floor_number' => 'integer|max:4|min:1',
+            'floor_name' => 'string|max:50|min:1',
+            'room_number' => 'integer|required|max:4|min:1',
+            'room_name' => 'integer|max:50|min:1',
+        ]);
+
+        if ($validator->fails()) {
+            return json_encode(array(
+                'success' => false,
+                'message' => $validator->errors()->all()
+            ));
+        }
+
+        if (Room::where('room_number', $request->input('room_number'))->where('building', $request->input('building'))->get()->first() ) {
+            if (Room::where('room_number', $request->input('room_number'))->where('building', $request->input('building'))->update($request->input())) {
+                return json_encode(array(
+                    'success' => true,
+                    'message' => 'update'
+                ));
+            } else {
+                return json_encode(array(
+                    'success' => false,
+                    'message' => 'Could not update'
+                ));
+            }
+
+        } else {
+            $model = new Room();
+
+            foreach ($request->input() as $key => $value) {
+                $model->$key = $value;
+            }
+
+            if ($model->save()) {
+                return json_encode(array(
+                    'success' => true,
+                    'message' => 'create'
+                ));
+            } else {
+                return json_encode(array(
+                    'success' => false,
+                    'message' => $model->errors()->all()
+                ));
+            }
+        }
+    }
+
 }
