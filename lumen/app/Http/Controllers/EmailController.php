66b4582fbe80376b6d02a2e3894851870979ceb0
Alex Markessinis <markea@almyz125.com>
2015-10-19 22:22:14 -0400

    added post method to email

--- lumen/app/Http/Controllers/EmailController.php
+++ lumen/app/Http/Controllers/EmailController.php
@@ -63,4 +63,56 @@ class EmailController extends BaseController
         }
     }
 
+    /**
+     * @param Request $request
+     * @return string
+     */
+    public function post(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'user' => 'integer|required|max:11|min:1',
+            'email' => 'email|required|max:60|min:7|unique:emails',
+        ]);
+
+        if ($validator->fails()) {
+            return json_encode(array(
+                'success' => false,
+                'message' => $validator->errors()->all()
+            ));
+        }
+
+        if (Email::where('email', $request->input('email'))->get()->first()) {
+            if (Email::where('email', $request->input('email'))->update($request->input())) {
+                return json_encode(array(
+                    'success' => true,
+                    'message' => 'update'
+                ));
+            } else {
+                return json_encode(array(
+                    'success' => false,
+                    'message' => 'Could not update'
+                ));
+            }
+
+        } else {
+            $model = new Email();
+
+            foreach ($request->input() as $key => $value) {
+                $model->$key = $value;
+            }
+
+            if ($model->save()) {
+                return json_encode(array(
+                    'success' => true,
+                    'message' => 'create'
+                ));
+            } else {
+                return json_encode(array(
+                    'success' => false,
+                    'message' => $model->errors()->all()
+                ));
+            }
+        }
+    }
 }
