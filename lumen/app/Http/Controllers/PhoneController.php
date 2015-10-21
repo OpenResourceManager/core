141c61c5fc2778d502dc727e22ef7afb8ebfa630
Alex Markessinis <markea@almyz125.com>
2015-10-21 11:48:50 -0400

    added auth and organized phone controller

--- lumen/app/Http/Controllers/PhoneController.php
+++ lumen/app/Http/Controllers/PhoneController.php
@@ -15,6 +15,7 @@ use App\APIKey;
 class PhoneController extends BaseController
 {
     /**
+     * @param Request $request
      * @param int $limit
      * @return string
      */
@@ -29,21 +30,23 @@ class PhoneController extends BaseController
     }
 
     /**
+     * @param Request $request
      * @param $id
      * @return string
      */
-    public function getById($id)
+    public function getById(Request $request, $id)
     {
-        $obj = Phone::where('id', $id)->get();
-        if ($obj && !is_null($obj) && !empty($obj) && sizeof($obj) > 0) {
-            return json_encode($obj);
+        $result = APIKey::testAPIKey($request, 'get');
+        if ($result[0]) {
+            $obj = Phone::where('id', $id)->get();
+            if ($obj && !is_null($obj) && !empty($obj) && sizeof($obj) > 0) {
+                return json_encode($obj);
+            } else {
+                return json_encode(
+                    array("success" => false, "error" => "NotFound"));
+            }
         } else {
-            return json_encode(
-                array(
-                    "success" => false,
-                    "error" => "NotFound"
-                )
-            );
+            return json_encode($result[1]);
         }
     }
 
@@ -53,51 +56,32 @@ class PhoneController extends BaseController
      */
     public function post(Request $request)
     {
-
-        $validator = Validator::make($request->all(), [
-            'user' => 'integer|required|max:11|min:1',
-            'number' => 'string|required|max:20|min:10|unique:phones',
-            'ext' => 'string|max:4|min:3|unique:phones'
-        ]);
-
-        if ($validator->fails()) {
-            return json_encode(array(
-                'success' => false,
-                'message' => $validator->errors()->all()
-            ));
-        }
-
-        if (Phone::where('number', $request->input('number'))->get()->first()) {
-            if (Phone::where('number', $request->input('number'))->update($request->input())) {
-                return json_encode(array(
-                    'success' => true,
-                    'message' => 'update'
-                ));
-            } else {
-                return json_encode(array(
-                    'success' => false,
-                    'message' => 'Could not update'
-                ));
+        $result = APIKey::testAPIKey($request, 'post');
+        if ($result[0]) {
+            $validator = Validator::make($request->all(), [
+                'user' => 'integer|required|max:11|min:1',
+                'number' => 'string|required|max:20|min:10|unique:phones',
+                'ext' => 'string|max:4|min:3|unique:phones'
+            ]);
+            if ($validator->fails()) {
+                return json_encode(array('success' => false, 'message' => $validator->errors()->all()));
             }
-
+            if (Phone::where('number', $request->input('number'))->get()->first()) {
+                if (Phone::where('number', $request->input('number'))->update($request->input())) {
+                    return json_encode(array('success' => true, 'message' => 'update'));
+                } else {
+                    return json_encode(array('success' => false, 'message' => 'Could not update'));
+                }
+            } else {
+                $model = new Phone();
+                foreach ($request->input() as $key => $value) {
+                    $model->$key = $value;
+                }
+                $save = $model->save() ? true : false;
+                return json_encode(array('success' => $save, 'message' => $save ? 'create' : $model->errors()->all()));
+            }
         } else {
-            $model = new Phone();
-
-            foreach ($request->input() as $key => $value) {
-                $model->$key = $value;
-            }
-
-            if ($model->save()) {
-                return json_encode(array(
-                    'success' => true,
-                    'message' => 'create'
-                ));
-            } else {
-                return json_encode(array(
-                    'success' => false,
-                    'message' => $model->errors()->all()
-                ));
-            }
+            return json_encode($result[1]);
         }
     }
 
