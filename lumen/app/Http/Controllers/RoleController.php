2425e44a71c67267a95a8204f843ec3d54ab586d
Alex Markessinis <markea@almyz125.com>
2015-10-21 12:09:24 -0400

    added auth and organized role controller

--- lumen/app/Http/Controllers/RoleController.php
+++ lumen/app/Http/Controllers/RoleController.php
@@ -16,6 +16,7 @@ class RoleController extends BaseController
 {
 
     /**
+     * @param Request $request
      * @param int $limit
      * @return string
      */
@@ -30,40 +31,42 @@ class RoleController extends BaseController
     }
 
     /**
+     * @param Request $request
      * @param $id
      * @return string
      */
-    public function getById($id)
+    public function getById(Request $request, $id)
     {
-        $obj = Role::where('id', $id)->get();
-        if ($obj && !is_null($obj) && !empty($obj) && sizeof($obj) > 0) {
-            return json_encode($obj);
+        $result = APIKey::testAPIKey($request, 'get');
+        if ($result[0]) {
+            $obj = Role::where('id', $id)->get();
+            if ($obj && !is_null($obj) && !empty($obj) && sizeof($obj) > 0) {
+                return json_encode($obj);
+            } else {
+                return json_encode(array("success" => false, "error" => "NotFound"));
+            }
         } else {
-            return json_encode(
-                array(
-                    "success" => false,
-                    "error" => "NotFound"
-                )
-            );
+            return json_encode($result[1]);
         }
     }
 
     /**
+     * @param Request $request
      * @param $code
      * @return string
      */
-    public function getByCode($code)
+    public function getByCode(Request $request, $code)
     {
-        $obj = Role::where('code', $code)->get();
-        if ($obj && !is_null($obj) && !empty($obj) && sizeof($obj) > 0) {
-            return json_encode($obj);
+        $result = APIKey::testAPIKey($request, 'get');
+        if ($result[0]) {
+            $obj = Role::where('code', $code)->get();
+            if ($obj && !is_null($obj) && !empty($obj) && sizeof($obj) > 0) {
+                return json_encode($obj);
+            } else {
+                return json_encode(array("success" => false, "error" => "NotFound"));
+            }
         } else {
-            return json_encode(
-                array(
-                    "success" => false,
-                    "error" => "NotFound"
-                )
-            );
+            return json_encode($result[1]);
         }
     }
 
@@ -73,51 +76,32 @@ class RoleController extends BaseController
      */
     public function post(Request $request)
     {
+        $result = APIKey::testAPIKey($request, 'post');
+        if ($result[0]) {
+            $validator = Validator::make($request->all(), [
+                'code' => 'string|required|max:50|min:3|unique:programs',
+                'name' => 'string|required|max:50|min:3|unique:programs',
 
-        $validator = Validator::make($request->all(), [
-            'code' => 'string|required|max:50|min:3|unique:programs',
-            'name' => 'string|required|max:50|min:3|unique:programs',
-
-        ]);
-
-        if ($validator->fails()) {
-            return json_encode(array(
-                'success' => false,
-                'message' => $validator->errors()->all()
-            ));
-        }
-
-        if (Role::where('code', $request->input('code'))->get()->first()) {
-            if (Role::where('code', $request->input('code'))->update($request->input())) {
-                return json_encode(array(
-                    'success' => true,
-                    'message' => 'update'
-                ));
-            } else {
-                return json_encode(array(
-                    'success' => false,
-                    'message' => 'Could not update'
-                ));
+            ]);
+            if ($validator->fails()) {
+                return json_encode(array('success' => false, 'message' => $validator->errors()->all()));
             }
-
+            if (Role::where('code', $request->input('code'))->get()->first()) {
+                if (Role::where('code', $request->input('code'))->update($request->input())) {
+                    return json_encode(array('success' => true, 'message' => 'update'));
+                } else {
+                    return json_encode(array('success' => false, 'message' => 'Could not update'));
+                }
+            } else {
+                $model = new Role();
+                foreach ($request->input() as $key => $value) {
+                    $model->$key = $value;
+                }
+                $save = $model->save() ? true : false;
+                return json_encode(array('success' => $save, 'message' => $save ? 'create' : $model->errors()->all()));
+            }
         } else {
-            $model = new Role();
-
-            foreach ($request->input() as $key => $value) {
-                $model->$key = $value;
-            }
-
-            if ($model->save()) {
-                return json_encode(array(
-                    'success' => true,
-                    'message' => 'create'
-                ));
-            } else {
-                return json_encode(array(
-                    'success' => false,
-                    'message' => $model->errors()->all()
-                ));
-            }
+            return json_encode($result[1]);
         }
     }
 
