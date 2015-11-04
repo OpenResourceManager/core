ff97f8523c170a2e63fe51fe30c9121158a58c53
Alex Markessinis <markea@almyz125.com>
2015-11-03 22:58:05 -0500

    hotfix: fixing soft delete cascade

--- lumen/app/Building.php
+++ lumen/app/Building.php
@@ -10,7 +10,7 @@
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Building extends Eloquent
+class Building extends Model
 {
     use SoftDeletes;
     protected $table = 'buildings';
