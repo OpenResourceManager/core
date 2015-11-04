e0ed1a429d7ff8b491e76f0fa4759150ae890f29
Alex Markessinis <markea@almyz125.com>
2015-11-04 18:50:40 -0500

    routes have been converted from lumen style to laravel style.

--- laravel/app/Http/routes.php
+++ laravel/app/Http/routes.php
@@ -25,7 +25,6 @@ Route::group(['prefix' => 'v1'], function () {
     });
 
     Route::group(['prefix' => 'user'], function () {
-
         Route::post('/', 'UserController@post');
 
         Route::get('id/{id}', 'UserController@getByID');
@@ -33,39 +32,90 @@ Route::group(['prefix' => 'v1'], function () {
 
         Route::get('/', 'UserController@get');
         Route::get('/{limit}', 'UserController@get');
-
     });
 
     Route::group(['prefix' => 'role'], function () {
+        Route::post('/', 'RoleController@post');
 
+        Route::get('id/{id}', 'RoleController@getByID');
+        Route::get('code/{code}', 'RoleController@getByCode');
+
+        Route::get('/', 'RoleController@get');
+        Route::get('/{limit}', 'RoleController@get');
     });
 
     Route::group(['prefix' => 'building'], function () {
+        Route::post('/', 'BuildingController@post');
 
+        Route::delete('/', 'BuildingController@del');
+
+        Route::get('id/{id}', 'BuildingController@getByID');
+        Route::get('code/{code}', 'BuildingController@getByCode');
+        Route::get('campus/{campusId}', 'BuildingController@getByCampus');
+
+        Route::get('/', 'BuildingController@get');
+        Route::get('/{limit}', 'BuildingController@get');
     });
 
     Route::group(['prefix' => 'campus'], function () {
+        Route::post('/', 'CampusController@post');
 
+        Route::delete('/', 'CampusController@del');
+
+        Route::get('id/{id}', 'CampusController@getByID');
+        Route::get('code/{code}', 'CampusController@getByCode');
+
+        Route::get('/', 'CampusController@get');
+        Route::get('/{limit}', 'CampusController@get');
     });
 
     Route::group(['prefix' => 'program'], function () {
+        Route::post('/', 'ProgramController@post');
 
+        Route::get('id/{id}', 'ProgramController@getByID');
+        Route::get('code/{code}', 'ProgramController@getByCode');
+        Route::get('department/{departmentId}', 'ProgramController@getByDepartment');
+
+        Route::get('/', 'ProgramController@get');
+        Route::get('/{limit}', 'ProgramController@get');
     });
 
     Route::group(['prefix' => 'department'], function () {
+        Route::post('/', 'DepartmentController@post');
 
+        Route::get('id/{id}', 'DepartmentController@getByID');
+        Route::get('code/{code}', 'DepartmentController@getByCode');
+
+        Route::get('/', 'DepartmentController@get');
+        Route::get('/{limit}', 'DepartmentController@get');
     });
 
     Route::group(['prefix' => 'email'], function () {
+        Route::post('/', 'EmailController@post');
 
+        Route::get('id/{id}', 'EmailController@getByID');
+        Route::get('user/{id}', 'EmailController@getByUser');
+
+        Route::get('/', 'EmailController@get');
+        Route::get('/{limit}', 'EmailController@get');
     });
 
     Route::group(['prefix' => 'phone'], function () {
+        Route::post('/', 'PhoneController@post');
 
+        Route::get('id/{id}', 'PhoneController@getByID');
+
+        Route::get('/', 'PhoneController@get');
+        Route::get('/{limit}', 'PhoneController@get');
     });
 
     Route::group(['prefix' => 'room'], function () {
+        Route::post('/', 'RoomController@post');
 
+        Route::get('id/{id}', 'RoomController@getByID');
+
+        Route::get('/', 'RoomController@get');
+        Route::get('/{limit}', 'RoomController@get');
     });
 
     Route::group(['prefix' => 'course'], function () {
