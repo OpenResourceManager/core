0780d2223a0deee18ba1ef8b9408a00d19a6254d
Alex Markessinis <markea@almyz125.com>
2015-11-04 15:29:58 -0500

    not veifying csrf

--- laravel/app/Http/Kernel.php
+++ laravel/app/Http/Kernel.php
@@ -15,9 +15,9 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
         \App\Http\Middleware\EncryptCookies::class,
         \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-        \Illuminate\Session\Middleware\StartSession::class,
-        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-        \App\Http\Middleware\VerifyCsrfToken::class,
+       // \Illuminate\Session\Middleware\StartSession::class,
+       // \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+       // \App\Http\Middleware\VerifyCsrfToken::class,
     ];
 
     /**
