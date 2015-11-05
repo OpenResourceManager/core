4c192df8a16da05e17f834e44ea3cce181f1c05d
Alex Markessinis <markea@almyz125.com>
2015-11-04 22:19:38 -0500

    fixing seeders

--- laravel/database/migrations/2015_06_23_193628_create_tables.php
+++ laravel/database/migrations/2015_06_23_193628_create_tables.php
@@ -140,7 +140,7 @@ class CreateTables extends Migration
         });
 
         Schema::table('rooms', function (Blueprint $table) {
-            $table->dropForeign('rooms_use_idr_foreign');
+            $table->dropForeign('rooms_user_id_foreign');
             $table->dropForeign('rooms_building_id_foreign');
         });
 
