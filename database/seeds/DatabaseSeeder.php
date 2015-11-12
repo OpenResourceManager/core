23b7af03439be7a08b01f46974a15cb364472ae0
Alex Markessinis <markea@almyz125.com>
2015-11-11 20:18:48 -0500

    truncating tables in seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use App\Model\Type\Role;
+use App\Model\Type\Campus;
+use App\Model\Type\Building;
+use App\Model\Type\Department;
+use App\Model\Type\Course;
+use App\Model\Type\Community;
+use App\Model\Record\User_Record;
+use App\Model\Record\Email_Record;
+use App\Model\Record\Phone_Record;
+use App\Model\Record\Room_Record;
+use App\Model\Record\Department_Record;
+use App\Model\Record\API_Key_Record;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +25,20 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         Model::unguard();
+
+        Role::truncate();
+        Campus::truncate();
+        Building::truncate();
+        Department::truncate();
+        Course::truncate();
+        Community::truncate();
+
+        User_Record::truncate();
+        Email_Record::truncate();
+        Phone_Record::truncate();
+        Room_Record::truncate();
+        Department_Record::truncate();
+        API_Key_Record::truncate();
 
         $this->call('RoleTableSeeder');
         $this->call('CampusTableSeeder');
