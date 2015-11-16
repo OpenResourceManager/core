86f7ed59b018e9d8d5cbfdd84014c77646075ef4
Alex Markessinis <markea@almyz125.com>
2015-11-16 11:50:18 -0500

    moved helpers to a trait

--- tests/helpers/ApiTester.php
+++ tests/helpers/ApiTester.php
@@ -19,11 +19,6 @@ abstract class ApiTester extends TestCase
     protected $fake;
 
     /**
-     * @var int
-     */
-    protected $times = 1;
-
-    /**
      * ApiTester constructor.
      * @param $fake
      */
@@ -37,33 +32,6 @@ abstract class ApiTester extends TestCase
         parent::setUp();
 
         Artisan::call('migrate:refresh');
-    }
-
-    /**
-     * @param $count
-     * @return $this
-     */
-    public function times($count)
-    {
-        $this->times = $count;
-        return $this;
-    }
-
-    /**
-     * @param $type
-     * @param array $fields
-     */
-    protected function make($type, array $fields = [])
-    {
-        while ($this->times--) {
-            $stub = array_merge($this->getStub(), $fields);
-            $type::create($stub);
-        }
-    }
-
-    protected function getStub()
-    {
-        throw new BadMethodCallException('Create your own getStub method to declare your fields.');
     }
 
     /**
