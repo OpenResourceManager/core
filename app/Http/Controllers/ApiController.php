ea2fa0b2a32dc2d8a42ce2904e869eee4b19e7bf
Alex Markessinis <markea@almyz125.com>
2015-11-16 14:54:12 -0500

    created respond with pagination

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 
-use Illuminate\Support\Facades\Request;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
 
 class ApiController extends Controller
