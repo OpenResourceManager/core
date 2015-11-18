106ab1997deac96e07b7c15e1c616a59a7a712ad
Alex Markessinis <markea@almyz125.com>
2015-11-17 23:22:55 -0500

    user rooms by sage id

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -156,7 +156,7 @@ class RoomController extends ApiController
      */
     public function userRoomsByUserId($user_id)
     {
-        $result = User::where('user_identifier', $user_id)->get()->rooms();
+        $result = User::where('user_identifier', $user_id)->get()->rooms;
         return $this->respondWithSuccess($this->roomTransformer->transformCollection($result));
     }
 }
