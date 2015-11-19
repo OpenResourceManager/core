793f64e43d032466c1dd8c819d8507d3ba1e41ee
Alex Markessinis <markea@almyz125.com>
2015-11-18 22:09:13 -0500

    trying to add auth to construct

--- app/Http/Controllers/BuildingController.php
+++ app/Http/Controllers/BuildingController.php
@@ -24,7 +24,7 @@ class BuildingController extends ApiController
      */
     function __construct(Request $request, BuildingTransformer $buildingTransformer)
     {
-        if ($this->isAuthorized($request)) return $this->respondNotAuthorized();
+        if (!$this->isAuthorized($request)) return $this->respondNotAuthorized();
         $this->buildingTransformer = $buildingTransformer;
     }
 
