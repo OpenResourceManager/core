4b80e7621c7ac5041195c89b5cee01991de25ee8
Alex Markessinis <markea@almyz125.com>
2015-11-20 11:13:35 -0500

    fixing eger loaded relarions

--- app/Model/Building.php
+++ app/Model/Building.php
@@ -25,7 +25,7 @@ class Building extends Model
 
     public function users()
     {
-        return $this->hasManyThrough('App\Model\User', 'App\Model\Room', 'building_id', 'user_id', 'id');
+        return $this->rooms()->with('users');
     }
 
     public function campus()
