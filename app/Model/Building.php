4104966e0e004ffc7d5b0046183987409da38cfd
Alex Markessinis <markea@almyz125.com>
2016-01-05 12:17:14 -0500

    added get users by associated role code

--- app/Model/Building.php
+++ app/Model/Building.php
@@ -26,7 +26,7 @@ class Building extends Model
 
     public function users()
     {
-        return $this->hasManyThrough('App\Model\User', 'App\Model\Room', 'building_id', 'id', 'room_id');
+        return $this->hasManyThrough('App\Model\User', 'App\Model\Room', 'building_id', 'id', 'id');
     }
 
     public function campus()
