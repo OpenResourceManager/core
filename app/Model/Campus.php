d210cdab6d5c61e1f0345cfb8f4d11214c4f0f87
Alex Markessinis <markea@almyz125.com>
2016-01-05 13:17:41 -0500

    trying to get users by campus

--- app/Model/Campus.php
+++ app/Model/Campus.php
@@ -31,7 +31,7 @@ class Campus extends BaseModel
 
     public function users()
     {
-        return $this->rooms()->with('users')->get();
+        return $this->rooms()->with('users');
     }
 
     public function communities()
