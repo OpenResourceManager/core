d1a4de48496f4668fd6c6a701f8b517af06b2501
Alex Markessinis <markea@almyz125.com>
2016-01-05 19:58:20 -0500

    using pivot table to create belongs to relationships for users and campuses

--- database/migrations/2016_01_06_001550_create_campus_user_pivot_table.php
+++ database/migrations/2016_01_06_001550_create_campus_user_pivot_table.php
@@ -14,7 +14,7 @@ class CreateCampusUserPivotTable extends Migration
     {
         Schema::create('campus_user', function (Blueprint $table) {
             $table->integer('campus_id')->unsigned()->index();
-            $table->foreign('campus_id')->references('campus_id')->on('buildings')->onDelete('cascade');
+            $table->foreign('campus_id')->references('id')->on('campus')->onDelete('cascade');
             $table->integer('user_id')->unsigned()->index();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->primary(['campus_id', 'user_id']);
