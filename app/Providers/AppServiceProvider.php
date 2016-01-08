64a730199e44560c72ca597d71782ad740e4ede2
Alex Markessinis <markea@almyz125.com>
2016-01-08 14:21:00 -0500

    trying to cascade deletions from campus down to rooms from rooms to user campus relationship

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,14 +2,6 @@
 
 namespace App\Providers;
 
-use App\Model\Building;
-use App\Model\Campus;
-use App\Model\Course;
-use App\Model\Department;
-use App\Model\Room;
-use App\Model\User;
-use Illuminate\Database\Eloquent\Relations\Pivot;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,77 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Pivot::saving(function ($pivot) {
-            if ($pivot->getTable() == 'course_user') {
-                $course = Course::findOrFail($pivot->course_id);
-                $user = User::findOrFail($pivot->user_id);
-                $department = Department::findOrFail($course->department_id);
-                if (!$user->departments->contains($department->id)) {
-                    $user->departments()->attach($department);
-                }
-            }
-        });
 
-
-        // Adds a user campus record when a user is assigned a room.
-        Room::created(function ($room) {
-            $user_id = $room->user_id;
-            $building = Building::find($room->building_id);
-            $campus_id = $building->campus_id;
-
-            $user = User::findOrFail($user_id);
-            $campus = Campus::findOrFail($campus_id);
-
-            if (!$user->campuses->contains($campus->id)) {
-                $user->campuses()->attach($campus);
-            }
-
-        });
-
-        // set deleting event for campus. Should delete all children buildings.
-        Campus::deleting(function ($campus) {
-            $campus->buildings()->delete();
-            Building::deleting(function ($building) {
-                $building->rooms()->delete();
-            });
-            Room::deleting(function ($room) {
-                $user_id = $room->user_id;
-                $building = Building::find($room->building_id);
-                $campus_id = $building->campus_id;
-
-                $user = User::findOrFail($user_id);
-                $campus = Campus::findOrFail($campus_id);
-
-                if (!$user->campuses->contains($campus->id)) {
-                    $user->campuses()->detach($campus);
-                }
-            });
-        });
-
-        // set deleting event for building. Should delete all children rooms.
-        Building::deleting(function ($building) {
-            $building->rooms()->delete();
-        });
-
-        // Adds a user campus record when a user is assigned a room.
-        Room::deleting(function ($room) {
-            $user_id = $room->user_id;
-            $building = Building::find($room->building_id);
-            $campus_id = $building->campus_id;
-
-            $user = User::findOrFail($user_id);
-            $campus = Campus::findOrFail($campus_id);
-
-            if (!$user->campuses->contains($campus->id)) {
-                $user->campuses()->detach($campus);
-            }
-        });
-
-        User::deleting(function ($user) {
-            $user->emails()->delete();
-            $user->phones()->delete();
-            $user->rooms()->delete();
-        });
     }
 
     /**
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,13 @@
 
 namespace App\Providers;
 
+use App\Model\Building;
+use App\Model\Campus;
+use App\Model\Course;
+use App\Model\Department;
+use App\Model\Room;
+use App\Model\User;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 use Illuminate\Contracts\Events\Dispatcher as DispatcherContract;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
@@ -28,6 +35,77 @@ class EventServiceProvider extends ServiceProvider
     {
         parent::boot($events);
 
-        //
+        Pivot::saving(function ($pivot) {
+            if ($pivot->getTable() == 'course_user') {
+                $course = Course::findOrFail($pivot->course_id);
+                $user = User::findOrFail($pivot->user_id);
+                $department = Department::findOrFail($course->department_id);
+                if (!$user->departments->contains($department->id)) {
+                    $user->departments()->attach($department);
+                }
+            }
+        });
+
+
+        // Adds a user campus record when a user is assigned a room.
+        Room::created(function ($room) {
+            $user_id = $room->user_id;
+            $building = Building::find($room->building_id);
+            $campus_id = $building->campus_id;
+
+            $user = User::findOrFail($user_id);
+            $campus = Campus::findOrFail($campus_id);
+
+            if (!$user->campuses->contains($campus->id)) {
+                $user->campuses()->attach($campus);
+            }
+
+        });
+
+        // set deleting event for campus. Should delete all children buildings.
+        Campus::deleting(function ($campus) {
+            $campus->buildings()->delete();
+            Building::deleting(function ($building) {
+                $building->rooms()->delete();
+            });
+            Room::deleting(function ($room) {
+                $user_id = $room->user_id;
+                $building = Building::find($room->building_id);
+                $campus_id = $building->campus_id;
+
+                $user = User::findOrFail($user_id);
+                $campus = Campus::findOrFail($campus_id);
+
+                if (!$user->campuses->contains($campus->id)) {
+                    $user->campuses()->detach($campus);
+                }
+            });
+        });
+
+        // set deleting event for building. Should delete all children rooms.
+        Building::deleting(function ($building) {
+            $building->rooms()->delete();
+        });
+
+        // Adds a user campus record when a user is assigned a room.
+        Room::deleting(function ($room) {
+            $user_id = $room->user_id;
+            $building = Building::find($room->building_id);
+            $campus_id = $building->campus_id;
+
+            $user = User::findOrFail($user_id);
+            $campus = Campus::findOrFail($campus_id);
+
+            if (!$user->campuses->contains($campus->id)) {
+                $user->campuses()->detach($campus);
+            }
+        });
+
+        User::deleting(function ($user) {
+            $user->emails()->delete();
+            $user->phones()->delete();
+            $user->rooms()->delete();
+        });
+
     }
 }
