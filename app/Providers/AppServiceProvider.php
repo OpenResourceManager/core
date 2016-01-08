a26f2d7669f6a47c2f2fff00a53ee144a060aa38
Alex Markessinis <markea@almyz125.com>
2016-01-08 13:41:30 -0500

    trying to automatically associate users with departments based on course, same for user to campus based on room

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Pivot::saving(function ($pivot) {
+            if ($pivot->getTable() == 'course_user') {
+                $course = Course::findOrFail($pivot->course_id);
+                $user = User::findOrFail($pivot->user_id);
+                $department = Department::findOrFail($course->department_id);
+                if (!$user->departments->contains($department->id)) {
+                    $user->departments()->attach($department);
+                }
+            }
+        });
+
         // set deleting event for building. Should delete all children rooms.
         Building::deleting(function ($building) {
             $building->rooms()->delete();
@@ -39,28 +50,26 @@ class AppServiceProvider extends ServiceProvider
                 $user->campuses()->attach($campus);
             }
 
-            /* if (!DB::table('campus_user')->where('campus_id', $campus_id)->where('user_id', $user_id)->first()) {
-                 DB::table('campus_user')->insert(['campus_id' => $campus_id, 'user_id' => $user_id]);
-             } */
+        });
+
+        // Adds a user campus record when a user is assigned a room.
+        Room::deleting(function ($room) {
+            $user_id = $room->user_id;
+            $building = Building::find($room->building_id);
+            $campus_id = $building->campus_id;
+
+            $user = User::findOrFail($user_id);
+            $campus = Campus::findOrFail($campus_id);
+
+            if (!$user->campuses->contains($campus->id)) {
+                $user->campuses()->attach($campus);
+            }
         });
 
         // set deleting event for campus. Should delete all children buildings.
         Campus::deleting(function ($campus) {
-            foreach ($campus->rooms() as $room) {
-                $room->delete();
-            }
+            $campus->rooms()->delete();
             $campus->buildings()->delete();
-        });
-
-        Pivot::created(function ($pivot) {
-            if ($pivot->getTable() == 'course_user') {
-                $course = Course::findOrFail($pivot->course_id);
-                $user = User::findOrFail($pivot->user_id);
-                $department = Department::findOrFail($course->department_id);
-                if (!$user->departments->contains($department->id)) {
-                    $user->departments()->attach($department);
-                }
-            }
         });
 
         User::deleting(function ($user) {
