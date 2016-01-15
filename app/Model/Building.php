1ca15c13081e1f60199dc9982040a955dec89c95
Alex Markessinis <markea@almyz125.com>
2016-01-15 16:58:51 -0500

    trying to fix building users

--- app/Model/Building.php
+++ app/Model/Building.php
@@ -28,7 +28,7 @@ class Building extends BaseModel
     {
         $users = array();
         foreach ($this->rooms()->get() as $room) {
-            array_merge($users, $room->users()->get()->all());
+            array_merge($users, $room->users()->toArray());
         }
         return Collection::make($users);
     }
