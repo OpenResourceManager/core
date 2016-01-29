c0fbfa125766e7f83490cc7b779e0c3352d035fb
Alex Markessinis <markea@almyz125.com>
2016-01-29 16:49:46 -0500

    added mobile caiiers, this fixes #51

--- database/migrations/2015_11_13_170855_create_mobile_carriers_table.php
+++ database/migrations/2015_11_13_170855_create_mobile_carriers_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCoursesTable extends Migration
+class CreateMobileCarriersTable extends Migration
 {
     /**
      * Run the migrations.
