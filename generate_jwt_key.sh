70b6d9799b33681891b322bd47cb1305a46744e6
Alex Markessinis <markea@sage.edu>
2016-11-02 12:14:12 -0400

    updated genrate_jwt_key to support osx and linux. seems like sed acts differently on different platforms.

--- generate_jwt_key.sh
+++ generate_jwt_key.sh
@@ -2,4 +2,11 @@
 
 OUT=$(php artisan jwt:generate);
 KEY=$(echo $OUT | cut -d "[" -f2 | cut -d "]" -f1);
-sed -i '' "s,JWT_SECRET=changeme,JWT_SECRET=$KEY,g" .env
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    sed -i "s,JWT_SECRET=changeme,JWT_SECRET=$KEY,g" .env
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' "s,JWT_SECRET=changeme,JWT_SECRET=$KEY,g" .env
+else
+    sed -i "s,JWT_SECRET=changeme,JWT_SECRET=$KEY,g" .env
+fi
