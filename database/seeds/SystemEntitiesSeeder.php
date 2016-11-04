f106ebc4e6e4acb14861ca7a445aedc16901f964
Alex Markessinis <markea@sage.edu>
2016-11-03 21:37:48 -0400

    added more permissions

--- database/seeds/SystemEntitiesSeeder.php
+++ database/seeds/SystemEntitiesSeeder.php
@@ -22,79 +22,17 @@ class SystemEntitiesSeeder extends Seeder
     {
         Model::unguard();
 
-        $readDuty = Permission::create([
-            'name' => 'read-duty',
-            'display_name' => 'Read Duty',
-            'description' => 'Can read duty attributes.',
-        ]);
+        /**
+         *
+         *
+         * Permissions
+         *
+         *
+         */
 
-        $writeDuty = Permission::create([
-            'name' => 'write-duty',
-            'display_name' => 'Write Duty',
-            'description' => 'Can write duty attributes.',
-        ]);
-
-        $readCampus = Permission::create([
-            'name' => 'read-campus',
-            'display_name' => 'Read Campuses',
-            'description' => 'Can read campus attributes.',
-        ]);
-
-        $writeCampus = Permission::create([
-            'name' => 'write-campus',
-            'display_name' => 'Write Campuses',
-            'description' => 'Can write campus attributes.',
-        ]);
-
-        $readBuilding = Permission::create([
-            'name' => 'read-building',
-            'display_name' => 'Read Buildings',
-            'description' => 'Can read building attributes.',
-        ]);
-
-        $writeBuilding = Permission::create([
-            'name' => 'write-building',
-            'display_name' => 'Write Buildings',
-            'description' => 'Can write building attributes.',
-        ]);
-
-        $readRooms = Permission::create([
-            'name' => 'read-rooms',
-            'display_name' => 'Read Rooms',
-            'description' => 'Can read rooms attributes.',
-        ]);
-
-        $writeRooms = Permission::create([
-            'name' => 'write-rooms',
-            'display_name' => 'Write Rooms',
-            'description' => 'Can write rooms attributes.',
-        ]);
-
-        $readDepartments = Permission::create([
-            'name' => 'read-departments',
-            'display_name' => 'Read Departments',
-            'description' => 'Can read departments attributes.',
-        ]);
-
-        $writeDepartments = Permission::create([
-            'name' => 'write-departments',
-            'display_name' => 'Write Departments',
-            'description' => 'Can write departments attributes.',
-        ]);
-
-        $readCourses = Permission::create([
-            'name' => 'read-courses',
-            'display_name' => 'Read Courses',
-            'description' => 'Can read courses attributes.',
-        ]);
-
-        $writeCourses = Permission::create([
-            'name' => 'write-courses',
-            'display_name' => 'Write Courses',
-            'description' => 'Can write courses attributes.',
-        ]);
-
-
+        /**
+         * Account
+         */
         $readAccount = Permission::create([
             'name' => 'read-account',
             'display_name' => 'Read Account',
@@ -107,50 +45,241 @@ class SystemEntitiesSeeder extends Seeder
             'description' => 'Can write account attributes.',
         ]);
 
-        /**
-         * This permission applies to user related objects ie phone numbers, emails, address
-         */
-        $readAccountExtended = Permission::create([
-            'name' => 'read-account-ext',
-            'display_name' => 'Read Account Extended',
-            'description' => 'Can read extended account attributes.',
-        ]);
 
-        /**
-         * This permission applies to user related objects ie phone numbers, emails
-         */
-        $writeAccountExtended = Permission::create([
-            'name' => 'write-account-ext',
-            'display_name' => 'Write Account Extended',
-            'description' => 'Can write extended account attributes.',
-        ]);
-
-        /**
-         * This permission applies to user related objects ie: SSN & password
-         */
+        // This permission applies to user related objects ie: SSN & password
         $readClassifiedExtended = Permission::create([
             'name' => 'read-classified',
             'display_name' => 'Read Classified Attributes',
             'description' => 'Can write classified account attributes.',
         ]);
 
-        /**
-         * This permission applies to user related objects ie: SSN & password
-         */
+
+        // This permission applies to user related objects ie: SSN & password
         $writeClassifiedExtended = Permission::create([
             'name' => 'write-classified',
             'display_name' => 'Write Classified Attributes',
             'description' => 'Can write classified account attributes.',
         ]);
 
+        /**
+         * Address
+         */
+        $readAddress = Permission::create([
+            'name' => 'read-address',
+            'display_name' => 'Read Address',
+            'description' => 'Can read address attributes.',
+        ]);
 
-        $admin = Role::create([
+        $writeAddress = Permission::create([
+            'name' => 'write-address',
+            'display_name' => 'Write Address',
+            'description' => 'Can write address attributes.',
+        ]);
+
+        /**
+         * Building
+         */
+        $readBuilding = Permission::create([
+            'name' => 'read-building',
+            'display_name' => 'Read Buildings',
+            'description' => 'Can read building attributes.',
+        ]);
+
+        $writeBuilding = Permission::create([
+            'name' => 'write-building',
+            'display_name' => 'Write Buildings',
+            'description' => 'Can write building attributes.',
+        ]);
+
+        /**
+         * Campus
+         */
+        $readCampus = Permission::create([
+            'name' => 'read-campus',
+            'display_name' => 'Read Campuses',
+            'description' => 'Can read campus attributes.',
+        ]);
+
+        $writeCampus = Permission::create([
+            'name' => 'write-campus',
+            'display_name' => 'Write Campuses',
+            'description' => 'Can write campus attributes.',
+        ]);
+
+        /**
+         * Country
+         */
+        $readCountry = Permission::create([
+            'name' => 'read-country',
+            'display_name' => 'Read Country',
+            'description' => 'Can read country attributes.',
+        ]);
+
+        $writeCountry = Permission::create([
+            'name' => 'write-country',
+            'display_name' => 'Write Country',
+            'description' => 'Can write country attributes.',
+        ]);
+
+        /**
+         * Course
+         */
+        $readCourses = Permission::create([
+            'name' => 'read-courses',
+            'display_name' => 'Read Courses',
+            'description' => 'Can read courses attributes.',
+        ]);
+
+        $writeCourses = Permission::create([
+            'name' => 'write-courses',
+            'display_name' => 'Write Courses',
+            'description' => 'Can write courses attributes.',
+        ]);
+
+        /**
+         * Department
+         */
+        $readDepartments = Permission::create([
+            'name' => 'read-departments',
+            'display_name' => 'Read Departments',
+            'description' => 'Can read departments attributes.',
+        ]);
+
+        $writeDepartments = Permission::create([
+            'name' => 'write-departments',
+            'display_name' => 'Write Departments',
+            'description' => 'Can write departments attributes.',
+        ]);
+
+        /**
+         * Duty
+         */
+        $readDuty = Permission::create([
+            'name' => 'read-duty',
+            'display_name' => 'Read Duty',
+            'description' => 'Can read duty attributes.',
+        ]);
+
+        $writeDuty = Permission::create([
+            'name' => 'write-duty',
+            'display_name' => 'Write Duty',
+            'description' => 'Can write duty attributes.',
+        ]);
+
+        /**
+         * Email
+         */
+        $readEmail = Permission::create([
+            'name' => 'read-email',
+            'display_name' => 'Read Email',
+            'description' => 'Can read email attributes.',
+        ]);
+
+        $writeEmail = Permission::create([
+            'name' => 'write-email',
+            'display_name' => 'Write Email',
+            'description' => 'Can write email attributes.',
+        ]);
+
+        /**
+         * Mobile Carrier
+         */
+        $readMobileCarrier = Permission::create([
+            'name' => 'read-mobile-carrier',
+            'display_name' => 'Read Mobile Carrier',
+            'description' => 'Can read mobile carrier attributes.',
+        ]);
+
+        $writeMobileCarrier = Permission::create([
+            'name' => 'write-mobile-carrier',
+            'display_name' => 'Write Mobile Carrier',
+            'description' => 'Can write mobile carrier attributes.',
+        ]);
+
+        /**
+         * Mobile Phone
+         */
+        $readMobilePhone = Permission::create([
+            'name' => 'read-mobile-phone',
+            'display_name' => 'Read Mobile Phone',
+            'description' => 'Can read mobile phone attributes.',
+        ]);
+
+        $writeMobilePhone = Permission::create([
+            'name' => 'write-mobile-phone',
+            'display_name' => 'Write Mobile Phone',
+            'description' => 'Can write mobile phone attributes.',
+        ]);
+
+        /**
+         * Room
+         */
+        $readRooms = Permission::create([
+            'name' => 'read-rooms',
+            'display_name' => 'Read Rooms',
+            'description' => 'Can read rooms attributes.',
+        ]);
+
+        $writeRooms = Permission::create([
+            'name' => 'write-rooms',
+            'display_name' => 'Write Rooms',
+            'description' => 'Can write rooms attributes.',
+        ]);
+
+        /**
+         * State
+         */
+        $readState = Permission::create([
+            'name' => 'read-state',
+            'display_name' => 'Read State',
+            'description' => 'Can read states attributes.',
+        ]);
+
+        $writeState = Permission::create([
+            'name' => 'write-state',
+            'display_name' => 'Write State',
+            'description' => 'Can write states attributes.',
+        ]);
+
+
+        /**
+         *
+         *
+         * Roles
+         *
+         *
+         */
+
+        $adminRole = Role::create([
             'name' => 'admin',
             'display_name' => 'Administrator',
             'description' => 'Has all permissions.'
         ]);
 
-        $admin->attachPermissions(Permission::all());
+        $serviceRole = Role::create([
+            'name' => 'service-admin',
+            'display_name' => 'Service Admin',
+            'description' => 'Has all permissions. Used for service accounts.'
+        ]);
+
+        $accountAdmin = Role::create([
+            'name' => 'account-admin',
+            'display_name' => 'Account Admin',
+            'description' => 'Has permissions to manage account attributes, including classified attributes.'
+        ]);
+
+        $accountManager = Role::create([
+            'name' => 'account-manager',
+            'display_name' => 'Account Manager',
+            'description' => 'Has permissions to manage account attributes.'
+        ]);
+
+
+        $adminRole->attachPermissions(Permission::all());
+        $serviceRole->attachPermission(Permission::all());
+        $accountAdmin->attachPermission([
+
+        ]);
 
         $adminUser = User::create([
             'name' => 'Administrator',
