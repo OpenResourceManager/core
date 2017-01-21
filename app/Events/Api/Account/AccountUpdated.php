8cd05ae1196d78b278c3a3dd5fd252a3665b0120
Alex Markessinis <markea@sage.edu>
2017-01-21 14:03:30 -0500

    fixed account updated event

--- app/Events/Api/Account/AccountUpdated.php
+++ app/Events/Api/Account/AccountUpdated.php
@@ -5,6 +5,7 @@ namespace App\Events\Api\Account;
 use App\Events\Event;
 use Illuminate\Support\Facades\Log;
 use App\Http\Models\API\Account;
+use Illuminate\Support\Facades\Redis;
 
 class AccountUpdated extends Event
 {
@@ -14,19 +15,40 @@ class AccountUpdated extends Event
      */
     public function __construct(Account $account)
     {
+        Log::info('Account Updated:', [
+            'id' => $account->id,
+            'identifier' => $account->identifier,
+            'username' => $account->username,
+            'name' => $account->format_full_name(true)
+        ]);
+
         if (auth()->user()) {
 
-            Log::info('Account Updated:', [
-                'id' => $account->id,
-                'identifier' => $account->identifier,
-                'username' => $account->username,
-                'name' => $account->format_full_name(true)
-            ]);
+            $account->primary_duty = $account->primaryDuty;
 
             $trans = $account->toArray();
             $trans['name_full'] = $account->format_full_name(true);
+            if (array_key_exists('password', $trans)) {
+                $trans['password'] = decrypt($trans['password']);
+            }
             $trans['username'] = strtolower($trans['username']);
-            $this->account = json_encode($trans);
+
+            $data_to_secure = json_encode([
+                'data' => $trans,
+                'conf' => [
+                    'ldap' => ldap_config()
+                ]
+            ]);
+
+            $secure_data = encrypt_broadcast_data($data_to_secure);
+
+            $message = [
+                'event' => 'updated',
+                'type' => 'account',
+                'encrypted' => $secure_data
+            ];
+
+            Redis::publish('events', json_encode($message));
 
             history()->log(
                 'Account',
