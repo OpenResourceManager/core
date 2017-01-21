dcbd906cc383a55baa34d7b65ee0e16122f7be46
Alex Markessinis <markea@sage.edu>
2017-01-21 13:01:45 -0500

    added campus restored event.

---
+++ app/Events/Api/Campus/CampusRestored.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Events\Api\Campus;
+
+use App\Http\Models\API\Building;
+use App\Http\Models\API\Campus;
+use Illuminate\Broadcasting\Channel;
+use App\Events\Event;
+use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Log;
+
+class CampusRestored extends Event
+{
+
+    public function __construct(Campus $campus)
+    {
+        if (auth()->user()) {
+
+            Log::info('Campus Restored:', [
+                'id' => $campus->id,
+                'code' => $campus->code,
+                'label' => $campus->label
+            ]);
+
+            $campus->campus;
+
+            $data_to_secure = json_encode([
+                'data' => $campus->toArray(),
+                'conf' => [
+                    'ldap' => ldap_config()
+                ]
+            ]);
+
+            $secure_data = encrypt_broadcast_data($data_to_secure);
+
+            $message = [
+                'event' => 'restored',
+                'type' => 'campus',
+                'encrypted' => $secure_data
+            ];
+
+            Redis::publish('events', json_encode($message));
+
+            history()->log(
+                'Campus',
+                'restored a campus: ' . $campus->label() . '.',
+                $campus->id,
+                'university',
+                'bg-lime'
+            );
+        }
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return Channel|array
+     */
+//    public function broadcastOn()
+//    {
+//        return new PrivateChannel('account-events');
+//    }
+}
