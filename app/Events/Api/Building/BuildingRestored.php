b866f3b07d6b0425d0b02397e2d036ed2de852c8
Alex Markessinis <markea@sage.edu>
2017-01-21 12:34:03 -0500

    Created BuildingRestored event.

---
+++ app/Events/Api/Building/BuildingRestored.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Events\Api\Building;
+
+use App\Http\Models\API\Building;
+use Illuminate\Broadcasting\Channel;
+use App\Events\Event;
+use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Log;
+
+class BuildingRestored extends Event
+{
+
+    public function __construct(Building $building)
+    {
+        if (auth()->user()) {
+
+            Log::info('Building Restored:', [
+                'id' => $building->id,
+                'code' => $building->code,
+                'label' => $building->label
+            ]);
+
+            $building->campus;
+
+            $data_to_secure = json_encode([
+                'data' => $building->toArray(),
+                'conf' => [
+                    'ldap' => ldap_config()
+                ]
+            ]);
+
+            $secure_data = encrypt_broadcast_data($data_to_secure);
+
+            $message = [
+                'event' => 'restored',
+                'type' => 'building',
+                'encrypted' => $secure_data
+            ];
+
+            Redis::publish('events', json_encode($message));
+
+            history()->log(
+                'Building',
+                'restored a building: ' . $building->label() . '.',
+                $building->id,
+                'building',
+                'bg-lime'
+            );
+        }
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return Channel|array
+     */
+//    public function broadcastOn()
+//    {
+//        return new PrivateChannel('account-events');
+//    }
+}
