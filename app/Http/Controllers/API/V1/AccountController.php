d53caf1a1f45827747354653548e2ac07e8928d3
Alex Markessinis <markea125@gmail.com>
2017-11-16 19:14:17 -0500

    account updated fires on patch

--- app/Http/Controllers/API/V1/AccountController.php
+++ app/Http/Controllers/API/V1/AccountController.php
@@ -350,7 +350,7 @@ class AccountController extends ApiController
         // Update the account
         $item->update($data);
         // Fire the update event
-        event(new AccountUpdated($item));
+        event(new AccountUpdated(Account::where('identifier', $data['identifier'])->first()));
         // Get the updated account
         $item = $trans->transform($item->firstOrFail());
         // Return a response
